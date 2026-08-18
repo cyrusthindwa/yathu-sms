eab98b4a18adab404188bd145af5ad258cf88694
ChifundoGKauwa <kauwachifundo114@gmail.com>
2026-08-18 17:49:31 +0200

    feat:add sonner ui

---
+++ components/ui/sonner.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, ToasterProps } from "sonner";
+
+export function Toaster(props: ToasterProps) {
+  const { resolvedTheme } = useTheme();
+  return <Sonner theme={resolvedTheme === "dark" ? "dark" : "light"} richColors closeButton {...props} />;
+}
