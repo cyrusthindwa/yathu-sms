7517f16cf9dc47fae2266b27173b4b8239e777aa
ChifundoGKauwa <kauwachifundo114@gmail.com>
2026-08-18 17:32:16 +0200

    feat:add input component

---
+++ components/ui/input.tsx
@@ -0,0 +1,18 @@
+import { InputHTMLAttributes, ReactNode, forwardRef } from "react";
+import { cn } from "@/lib/utils";
+
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  leading?: ReactNode;
+  wrapperClassName?: string;
+};
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ leading, wrapperClassName, className, ...props }, ref) => (
+    <div data-slot="input-wrapper" className={cn("input-wrap", wrapperClassName)}>
+      {leading}
+      <input ref={ref} data-slot="input" className={className} {...props} />
+    </div>
+  ),
+);
+
+Input.displayName = "Input";
