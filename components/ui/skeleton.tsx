22db986e1e9fa2260c3da8214ce788d4f976844c
ChifundoGKauwa <kauwachifundo114@gmail.com>
2026-08-18 17:49:08 +0200

    feat:add skeleton ui

---
+++ components/ui/skeleton.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+export const Skeleton = React.forwardRef<HTMLDivElement, React.ComponentProps<"div">>(({ className, ...props }, ref) => <div ref={ref} data-slot="skeleton" aria-hidden="true" className={cn("skeleton", className)} {...props} />);
+Skeleton.displayName = "Skeleton";
