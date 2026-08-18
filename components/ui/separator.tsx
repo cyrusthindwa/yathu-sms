61888cbd7aa45146f3313c748c2feb909a290324
ChifundoGKauwa <kauwachifundo114@gmail.com>
2026-08-18 17:48:39 +0200

    feat:add separator ui

---
+++ components/ui/separator.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+type SeparatorProps = React.ComponentProps<"div"> & { orientation?: "horizontal" | "vertical" };
+export const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(({ className, orientation = "horizontal", ...props }, ref) => <div ref={ref} role="separator" aria-orientation={orientation} data-slot="separator" data-orientation={orientation} className={cn("separator", className)} {...props} />);
+Separator.displayName = "Separator";
